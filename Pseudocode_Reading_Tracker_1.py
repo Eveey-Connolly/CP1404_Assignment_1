4a5ec5d3a7bec27e4b2946d46ce7f2f00dd39370
eveey connolly <eveey.connolly@my.jcu.edu.au>
2018-08-20 20:01:34 +1000

    Began working on pseudocode

---
+++ Pseudocode_Reading_Tracker_1.py
@@ -0,0 +1,15 @@
+# Add docstring which includes my name, date, brief of how the reading tracker will work and github link
+# Display the number of books that currently exist in the CSV file when the code is first run and the user
+#  hasnt had any input
+# Create a menu that includes the options to a) list all the books, b) add a new book, c) mark a book as
+#  completed, d) quit
+# take a user input to see which option from the menu the user wants to pick
+# Create a while loop that performs the following operations until the user choose to quit:
+# Read and print the information contained in the CSV file (maybe store each line as a list)
+# Display the number of books in the file
+# Display the sum of the pages that is left to read
+# If option c) is chosen ask the user which book they want to mark as complete ( Error checking will need to be used)
+# If option b) is chosen ask the user for title, author,number of pages
+# Store this info in a list
+# Save this information to the CSV file so it updates
+# The list in the CSV file should be stored as [Title, Author, No. of Pages, Completed or still reading]
